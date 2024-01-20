b9ff18defd05e6bbcc12e78596aa843dc26dc98d
Mukil09 <mukilpoovaragvan@gmail.com>
2024-01-20 10:47:30 +0530

    Project completed successfully

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,78 @@
+import React, { useContext, useState } from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_logo from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
+
+function Navbar() {
+  let [menu, setMenu] = useState("shop");
+  let { getTotalCartItems } = useContext(ShopContext);
+
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+        <p>Shopify</p>
+      </div>
+
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link to="/" style={{ textDecoration: "none", color: "#626262" }}>
+            Shop
+          </Link>
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
+          <Link to="/mens" style={{ textDecoration: "none", color: "#626262" }}>
+            Men
+          </Link>
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("womens");
+          }}
+        >
+          <Link
+            to="/womens"
+            style={{ textDecoration: "none", color: "#626262" }}
+          >
+            Women
+          </Link>
+          {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link to="/kids" style={{ textDecoration: "none", color: "#626262" }}>
+            Kids
+          </Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_logo} alt="cart" />
+        </Link>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
